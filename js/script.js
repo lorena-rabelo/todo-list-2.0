7c2f49425278ba4ba8a61cf2d079fc78f6db587a
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2020-05-09 21:28:30 -0300

    Comecei a criar as funções que lidam com os itens

--- js/script.js
+++ js/script.js
@@ -25,15 +25,15 @@ form.addEventListener('submit', (e) => {
     })
   };
   input.focus();
+
 });
 
-
+texto.addEventListener('click', checkItem);
 // btnAllDone.addEventListener ('click', allDone);
 // btnRemoveAll.addEventListener ('click', removeAll);
 
 
 function createToDoItem(taskText) {
-
   const item = document.createElement('li');
   const texto = document.createElement('p');
   const trash = document.createElement('img');
@@ -45,3 +45,35 @@ function createToDoItem(taskText) {
   item.appendChild(trash);
   form.reset();
 };
+
+//toggle(), se a classe existir naquele elemento, ele a remove, se não existir, ele a adiciona.
+texto.addEventListener('click', function () {
+  texto.classList.toggle('checked');
+  console.log(texto);
+})
+
+
+// funções para tratar os itens da lista individualmente e coletivamente
+function handleItem(e){
+  const item = e.target;
+
+  if(item.tagName == 'li'){
+      checkItem(item);        // checa ou não a tarefa
+  } 
+
+  
+}
+
+
+function checkItem (item) {
+  item.classList.toggle('checked');
+}
+
+function deleteItem (item) {
+  
+}
+
+function deleteAll(){
+  todoTasks.innerHTML = '';
+  todoTasks.appendChild (sample);
+}
