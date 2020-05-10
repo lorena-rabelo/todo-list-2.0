54fa7517f4eebe7fe54b6d57c4f9c19243115310
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2020-05-10 19:47:47 -0300

    Função deletar todos - completa

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // variáveis globais utilizadas para manipular o HTML
-
 const input = document.getElementById('todoInput');
 const form = document.querySelector('form');
-const todoTasks = document.getElementById('todoTarefas');
+const todoTasks = document.getElementById('todoLista');
 const sample = document.getElementById('sample');  //só funcionou com ID 
 const addTask = document.getElementById('todoSubmit');
+const toDoContainer = document.querySelector('.todo__container');
 
 // variáveis globais utilizadas para excluir todos e selecionar todos
 const btnAllDone = document.querySelector('#todoMarcarTodos');
-const btnRemoveAll = document.querySelector('#todoRemoverTodoss');
-
+const btnRemoveAll = document.getElementById('todoRemoverTodos');
+console.log()
 // escutadores de eventos que utilizarei
 
 form.addEventListener('submit', (e) => {
@@ -28,16 +28,19 @@ form.addEventListener('submit', (e) => {
 
 });
 
-texto.addEventListener('click', checkItem);
+
+todoTasks.addEventListener('click', handleItem);
+// texto.addEventListener('click', checkItem);
 // btnAllDone.addEventListener ('click', allDone);
-// btnRemoveAll.addEventListener ('click', removeAll);
+btnRemoveAll.addEventListener('click', deleteAll);
 
 
 function createToDoItem(taskText) {
   const item = document.createElement('li');
   const texto = document.createElement('p');
   const trash = document.createElement('img');
-  sample.remove();
+  // sample.remove();
+  sample.style.display = 'none';
   trash.src = "icon-trash.svg";
   texto.innerText = taskText;
   todoTasks.appendChild(item);
@@ -47,33 +50,37 @@ function createToDoItem(taskText) {
 };
 
 //toggle(), se a classe existir naquele elemento, ele a remove, se não existir, ele a adiciona.
-texto.addEventListener('click', function () {
-  texto.classList.toggle('checked');
-  console.log(texto);
-})
+// texto.addEventListener('click', function () {
+//   texto.classList.toggle('checked');
+//   console.log(texto);
+// })
 
 
 // funções para tratar os itens da lista individualmente e coletivamente
-function handleItem(e){
+function handleItem(e) {
   const item = e.target;
 
-  if(item.tagName == 'li'){
-      checkItem(item);        // checa ou não a tarefa
-  } 
+  if (item.tagName == 'p') {
+    checkItem(item);        // checa ou não a tarefa
+  }
 
-  
+  if (item.tagName == 'img') {
+    deleteItem(item);        // apaga individualmente
+  }
+
 }
 
 
-function checkItem (item) {
+function checkItem(item) {
   item.classList.toggle('checked');
 }
 
-function deleteItem (item) {
-  
+function deleteItem(item) {
+  const deleteItem = item.parentNode;
+  todoTasks.removeChild(deleteItem);
 }
 
-function deleteAll(){
-  todoTasks.innerHTML = '';
-  todoTasks.appendChild (sample);
+function deleteAll() {
+  todoTasks.remove();
+  sample.style.display = 'block';
 }
